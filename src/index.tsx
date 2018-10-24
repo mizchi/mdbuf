7a0a83cd66e08b4c689434ee2f3bdd77435b329a
mizchi <miz404@gmail.com>
2018-10-24 19:53:11 +0900

    Rewrite with react

---
+++ src/index.tsx
@@ -0,0 +1,200 @@
+import "@babel/polyfill";
+import "github-markdown-css/github-markdown.css";
+import "katex/dist/katex.min.css";
+import "highlight.js/styles/default.css";
+
+import Proxy from "./lib/WorkerProxy";
+
+import React, { SyntheticEvent } from "react";
+import ReactDOM from "react-dom";
+
+type State = {
+  loaded: boolean;
+  wordCount: number;
+  raw: string;
+  html: string;
+  showPreview: boolean;
+};
+
+let proxy: any = null;
+
+let isComposing = false;
+const SHOW_PREVIEW_KEY = "show-preview";
+
+class App extends React.Component<{}, State> {
+  editorRef: React.RefObject<HTMLTextAreaElement> = React.createRef();
+  previewRef: React.RefObject<HTMLDivElement> = React.createRef();
+  previewContainerRef: React.RefObject<HTMLDivElement> = React.createRef();
+
+  state = {
+    raw: "",
+    html: "",
+    wordCount: 0,
+    loaded: false,
+    showPreview: true
+  };
+
+  async componentDidMount() {
+    const val = window.localStorage.getItem(SHOW_PREVIEW_KEY);
+    let showPreview: boolean = val ? JSON.parse(val) : true;
+
+    proxy = await new Proxy();
+    const lastState = await proxy.getLastState();
+
+    this.setState({
+      showPreview,
+      wordCount: Array.from(lastState.raw).length,
+      raw: lastState.raw,
+      html: lastState.html,
+      loaded: true
+    });
+
+    if (this.editorRef.current) {
+      this.editorRef.current.focus();
+    }
+
+    window.addEventListener("keydown", async ev => {
+      if (ev.ctrlKey && ev.key === "1") {
+        ev.preventDefault();
+        localStorage.setItem(SHOW_PREVIEW_KEY, String(!this.state.showPreview));
+        this.setState({ showPreview: !this.state.showPreview });
+        return;
+      }
+
+      if (ev.ctrlKey && ev.shiftKey && ev.key.toLowerCase() === "f") {
+        ev.preventDefault();
+
+        if (this.editorRef.current) {
+          const raw = this.editorRef.current.value;
+          const formatted = await proxy.format(raw);
+          this.editorRef.current.value = formatted;
+          const result = await proxy.compile(formatted);
+          this.setState({
+            html: result
+          });
+        }
+        return;
+      }
+
+      // keybind scroll
+
+      if (ev.ctrlKey && ev.shiftKey && ev.key.toLowerCase() === "j") {
+        ev.preventDefault();
+        if (this.previewContainerRef.current) {
+          const scrollTop = this.previewContainerRef.current.scrollTop;
+          this.previewContainerRef.current.scrollTop = scrollTop + 50;
+        }
+        return;
+      }
+
+      if (ev.ctrlKey && ev.shiftKey && ev.key.toLowerCase() === "k") {
+        ev.preventDefault();
+        if (this.previewContainerRef.current) {
+          const scrollTop = this.previewContainerRef.current.scrollTop;
+          this.previewContainerRef.current.scrollTop = scrollTop - 50;
+        }
+      }
+    });
+  }
+
+  async onChangeValue(ev: SyntheticEvent<HTMLTextAreaElement>) {
+    if (isComposing) {
+      return;
+    }
+    const rawValue = (ev.target as any).value;
+    this.setState({
+      wordCount: Array.from(rawValue).length
+    });
+
+    console.time("compile:worker");
+    const result = await proxy.compile(rawValue);
+    console.timeEnd("compile:worker");
+    this.setState({
+      html: result
+    });
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return (
+        <div style={{ padding: 18 }}>
+          <span style={{ color: "white" }}>Loading...</span>
+        </div>
+      );
+    }
+
+    return (
+      <>
+        <div style={{ flex: 1, height: "100vh" }}>
+          <div
+            style={{ width: "100%", display: "flex", justifyContent: "center" }}
+          >
+            <div
+              style={{
+                width: "100%",
+                maxWidth: "960px",
+                marginLeft: "auto",
+                marginRight: "auto"
+              }}
+            >
+              <textarea
+                ref={this.editorRef}
+                className="js-editor editor"
+                spellCheck={false}
+                defaultValue={this.state.raw}
+                onChange={this.onChangeValue.bind(this)}
+                onCompositionStart={() => {
+                  isComposing = true;
+                }}
+                onCompositionEnd={ev => {
+                  isComposing = false;
+                  this.onChangeValue(ev);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        {this.state.showPreview && (
+          <div
+            ref={this.previewContainerRef}
+            style={{
+              flex: 1,
+              height: "100vh",
+              overflowY: "auto",
+              background: "#eee"
+            }}
+          >
+            <div style={{ overflowY: "auto" }}>
+              <div
+                ref={this.previewRef}
+                className="markdown-body"
+                style={{ padding: "10px", lineHeight: "1.3em" }}
+                dangerouslySetInnerHTML={{ __html: this.state.html }}
+              />
+            </div>
+          </div>
+        )}
+        <div style={{ position: "absolute", right: "20px", bottom: "20px" }}>
+          <span style={{ fontFamily: "monospace", color: "cornflowerblue" }}>
+            {this.state.wordCount}
+          </span>
+          &nbsp;
+          <button
+            onClick={() => {
+              localStorage.setItem(
+                SHOW_PREVIEW_KEY,
+                String(!this.state.showPreview)
+              );
+
+              this.setState({ showPreview: !this.state.showPreview });
+            }}
+          >
+            👀
+          </button>
+        </div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
