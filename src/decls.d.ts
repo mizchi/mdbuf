96ea8764b72af9e17b002f85622ecafcdf1920a0
mizchi <miz404@gmail.com>
2019-09-07 22:04:04 +0900

    Remove unused files

--- src/decls.d.ts
+++ src/decls.d.ts
@@ -1,10 +1,5 @@
 declare module "remark";
 declare module "remark-*";
-declare module "comlinkjs";
 declare module "prettier/*";
-declare module "lodash.*";
-declare module "ipfs";
-declare module "date-fns/esm";
-// declare module "comlink";
 
 declare var ResizeObserver: any;
