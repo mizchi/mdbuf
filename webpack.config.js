cec56f05fe2ebae12ace34d3b9144e96471afe4d
mizchi <miz404@gmail.com>
2020-09-01 13:44:45 +0900

    Upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,64 @@
 const path = require("path");
-const {
-  CleanWebpackPlugin
-} = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HTMLPlugin = require("html-webpack-plugin");
 const WorkerPlugin = require("worker-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
-const {
-  GenerateSW
-} = require("workbox-webpack-plugin");
+const { GenerateSW } = require("workbox-webpack-plugin");
 const pkg = require("./package");
 
 const tsLoader = {
   loader: "ts-loader",
   options: {
-    transpileOnly: true
-  }
+    transpileOnly: true,
+  },
 };
 
 module.exports = (env, argv) => ({
   entry: {
-    main: path.join(__dirname, "src/main")
+    main: path.join(__dirname, "src/main"),
   },
   output: {
     globalObject: "self",
     filename: "[name].js",
-    chunkFilename: "[name].[id].[contenthash].js"
+    chunkFilename: "[name].[id].[contenthash].js",
   },
   resolve: {
-    extensions: [".js", ".ts", ".tsx"]
+    extensions: [".js", ".ts", ".tsx"],
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.worker\.(js|ts)$/i,
-        use: [{
-          loader: 'comlink-loader',
-          options: {
-            singleton: true
-          }
-        }]
+        use: [
+          {
+            loader: "comlink-loader",
+            options: {
+              singleton: true,
+            },
+          },
+        ],
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
-        use: [{
-          loader: "url-loader",
-          options: {
-            limit: 8192
-          }
-        }]
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
       },
       {
         test: /\.tsx?/,
-        use: [tsLoader]
-      }
-    ]
+        use: [tsLoader],
+      },
+    ],
   },
 
   plugins: [
@@ -65,29 +66,34 @@ module.exports = (env, argv) => ({
     new CleanWebpackPlugin(),
     new WorkerPlugin(),
     new webpack.DefinePlugin({
-      "process.env.VERSION": JSON.stringify(pkg.version)
+      "process.env.VERSION": JSON.stringify(pkg.version),
     }),
     new HTMLPlugin({
       template: path.join(__dirname, "src/main/index.html"),
-      inject: false
+      inject: false,
     }),
-    new CopyPlugin([{
-        from: "src/manifest.webmanifest",
-        to: "manifest.webmanifest"
-      },
-      {
-        from: "src/assets",
-        to: "assets"
-      }
-    ]),
-    ...(argv.mode === "production" ? [
-      new GenerateSW({
-        // globDirectory: path.join(__dirname, "dist"),
-        // globPatterns: ["*.{html,js,css}"],
-        swDest: "service-worker.js",
-        clientsClaim: true,
-        skipWaiting: true
-      })
-    ] : [])
-  ]
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/manifest.webmanifest",
+          to: "manifest.webmanifest",
+        },
+        {
+          from: "src/assets",
+          to: "assets",
+        },
+      ],
+    }),
+    ...(argv.mode === "production"
+      ? [
+          new GenerateSW({
+            // globDirectory: path.join(__dirname, "dist"),
+            // globPatterns: ["*.{html,js,css}"],
+            swDest: "service-worker.js",
+            clientsClaim: true,
+            skipWaiting: true,
+          }),
+        ]
+      : []),
+  ],
 });
